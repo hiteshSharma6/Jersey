04bf8486623c86a21b84d156b6004ec8461a643f
Hitesh Sharma <hitesh6sharma@gmail.com>
2018-05-13 22:08:10 +0530

    Add files via upload

--- src/main/java/com/zapper/webapp/lzres/query/ConceptSQL.java
+++ src/main/java/com/zapper/webapp/lzres/query/ConceptSQL.java
@@ -1,7 +1,12 @@
 package com.zapper.webapp.lzres.query;
 
+import com.zapper.webapp.lzres.utility.TableName;
+
 public interface ConceptSQL {
 
-	String GET_TOPIC_ID = null;
+	String GET_TOPIC_ID = ""
+			+ "SELECT concept_id"
+			+ "FROM "+ TableName.CONCEPT_TB
+			+ "WHERE topic_id = ?";
 
 }
--- src/main/java/com/zapper/webapp/lzres/query/EvaluationSQL.java
+++ src/main/java/com/zapper/webapp/lzres/query/EvaluationSQL.java
@@ -1,7 +1,15 @@
 package com.zapper.webapp.lzres.query;
 
+import com.zapper.webapp.lzres.utility.TableName;
+
 public interface EvaluationSQL {
 
-	String GET_EVALUATION_ID = null;
+	String GET_EVALUATION_ID = ""
+			+ " SELECT evaluation_id"
+			+ " FROM "+ TableName.EVALUATION_TB
+			+ " WHERE ("
+			+ "		topic_id = ?"
+			+ "		AND concept_id = ?"
+			+ " );";
 
 }
